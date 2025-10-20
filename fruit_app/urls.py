8cb5340a6d296df9f18d5c293cea0c5aa2c9727c
Abbas EL <abbas-el-mahmoud@hotmail.com>
2025-10-20 18:41:52 +0200

    Update urls.py

--- fruit_app/urls.py
+++ fruit_app/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from .views import send_fruits
 
-app_name = 'fruit_app'  # optional, aber hilfreich
+app_name = 'fruit_app' 
 
 urlpatterns = [
     path('', send_fruits),
